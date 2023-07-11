dfbcc6d8d05a19025514025c28254b34d5ac4b21
yeon <ohohooh@naver.com>
2023-07-11 23:04:23 +0900

    뷰 바인딩더 찾아볼 것

---
+++ Test31/app/src/main/java/com/jys/test31/CustomAdapter.kt
@@ -0,0 +1,54 @@
+package com.jys.test31
+
+import android.content.Context
+import android.graphics.drawable.DrawableContainer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.jys.test31.databinding.ActivityMainBinding
+import com.jys.test31.databinding.CustomListBinding
+
+class Data(val profile:Int, val name:String)
+
+class CustomAdapter(val context:Context, val DataList:ArrayList<Data>) : BaseAdapter() {
+
+    private var _binding: CustomListBinding? = null
+    private val binding get() = _binding!!
+
+    fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = CustomListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun getCount(): Int {
+        return DataList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return DataList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0L
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val view: View = LayoutInflater.from(context).inflate(R.layout.custom_list, null)
+        val profile = binding.ivCustom
+        val name = binding.tvCustom
+        val data = DataList[0]
+
+        profile.setImageResource(data.profile)
+        name.text = data.name
+        return view
+
+    }
+
+}
--- Test31/app/src/main/java/com/jys/test31/MainActivity.kt
+++ Test31/app/src/main/java/com/jys/test31/MainActivity.kt
@@ -8,16 +8,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.graphics.scaleMatrix
 import com.jys.test31.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    var DataList = arrayListOf(
+        Data(R.drawable.example, "0번"),
+        Data(R.drawable.example,"1번"),
+        Data(R.drawable.example,"2번"),
+        Data(R.drawable.example,"3번")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.listView.adapter = CustomAdapter(this, DataList)
+
+
+//        binding.listView.onItemClickListener=AdapterView.OnItemClickListener{parent,view,position,id->
+//            var selectItem = parent.getItemAtPosition(position)
+//            Toast.makeText(this,selectItem.toString(),Toast.LENGTH_SHORT).show()
+//        }
 
 
 
